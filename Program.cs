8741d388fa0e822a683a465db3820186c30e3bd4
Alexey <>
2024-01-25 13:13:20 +0300

    decided

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿class Person 
+{ 
+    public string Name { get; set; } 
+    public string Gender { get; set; } 
+    public int Age { get; set; } 
+    public List<Person> Parents { get; set; } 
+    public Person Spouse { get; set; } // добавлен атрибут spouse для хранения информации о супруге/супруге
+
+    public void SetSpouse(Person spouse) 
+    { 
+        Spouse = spouse; 
+        spouse.Spouse = this; // установить взаимную ссылку
+    } 
+} 
+ 
+class Family 
+{ 
+    public List<Person> Members { get; set; }
+
+    public Family() 
+    { 
+        Members = new List<Person>(); 
+    }
+
+    public void AddMember(Person person) 
+    { 
+        Members.Add(person); 
+    } 
+ 
+    public List<Person> GetCloseRelatives(Person person) 
+    { 
+        List<Person> relatives = new List<Person>(); 
+        // Логика для определения близких родственников, включая супруга/супругу
+        // Пример: relatives.Add(person.Spouse); 
+        return relatives; 
+    } 
+ 
+    public void PrintFamilyTree() 
+    { 
+        // Логика для вывода генеалогического дерева, включая супругов
+    } 
+}
